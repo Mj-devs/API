0f7f9c1bff56c2980ffd6c82b3392775ece748f2
Mj-Devs <developersholotan@gmail.com>
2025-01-29 09:22:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+
+// API Endpoint
+app.get("/", (req, res) => {
+  res.json({
+    email: "developersholotan@gmailcom",
+    current_datetime: new Date().toISOString(),
+    github_url: "https://github.com/Mj-devs/your-repo",
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`API running on port ${PORT}`);
+});
